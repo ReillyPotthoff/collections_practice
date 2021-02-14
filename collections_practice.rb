8ce306e1cef8d9dc199553318c77124db6b1d6df
reillypotthoff <reillypotthoff@gmail.com>
2021-02-14 12:36:27 -0600

    commit tree 2f89b9e600c5bd9f246c06faf68a1c92e63f014a

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,50 @@
+def sort_array_asc(num)
+    num.sort
+end
 
+def sort_array_desc(num)
+    desc_array = []
+    desc_array = num.sort.reverse
+end
+
+def sort_array_char_count(strings)
+    new_array = []
+    new_array = strings.sort {|a, b| a.length <=> b.length}
+end
+
+def swap_elements(a)
+    a[1], a[2] = a[2], a[1]
+    a
+end
+
+def reverse_array(num)
+    num.reverse
+end
+
+def kesha_maker(array)
+    kesha = []
+    array.each do |name|
+        name_array = name.split("")
+        name_array[2] = "$"
+        kesha << name_array.join
+    end
+    kesha
+end
+
+def find_a(words)
+    words.select { |x| x.start_with?("a")}
+end
+
+def sum_array(num)
+    num.sum
+end
+
+def add_s(words)
+    words.each_with_index.collect do |list, index|
+        if index == 1
+            list
+        else
+            list << "s"
+        end
+    end
+end 
